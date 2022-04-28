83722adb0f84dc49fe4285ec89f09e56952a03bf
HARAWATAN <104349703+HARAWATAN@users.noreply.github.com>
2022-04-28 21:46:34 +0900

    new page

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 var a = prompt("名前を入力してください");
-
-var msg = "ようこそ"+a+"さん";
+var now = new Date();
+var Hour = now.getHours();
+if(17 <= Hour || Hour < 5)
+{
+    var msg = "こんばんは"+a+"さん";
+}
+else if(Hour <= 5 || Hour < 12)
+{
+    var msg = "おはようございます"+a+"さん";
+}
+else if(Hour < 17)
+{
+    var msg = "こんにちは"+a+"さん";
+}
 var speed = 350;
 var move = 1;
 
